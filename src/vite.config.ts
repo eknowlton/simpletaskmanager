0ad0caac8f26f2ef12e028a75d03944e18361704
Ethan Knowlton <eknowlton@gmail.com>
2025-08-08 08:48:54 -0400

    removing zigggy in vendor references

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,6 +1,5 @@
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 /* @ts-ignore */
@@ -27,10 +26,5 @@ export default defineConfig({
     ],
     esbuild: {
         jsx: 'automatic',
-    },
-    resolve: {
-        alias: {
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
-        },
     },
 });
